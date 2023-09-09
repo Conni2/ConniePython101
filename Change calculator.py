453211a085156ad5adcee0b4b6bcccb9dcb1992f
Conni2 <connie.keum@gmail.com>
2023-09-10 00:00:18 +0900

    ChangeCal

---
+++ Change calculator.py
@@ -0,0 +1,26 @@
+#dollar change calculator
+def calculate_change(payment, cost):
+    change = payment - cost
+
+    hundred_count = change // 100 
+    fifty_count = (change % 100) // 50 
+    twenty_count = (change % 50) // 20
+    ten_count = (change % 20) // 10
+    five_count = (change % 10) // 5
+    two_count = (change % 5) // 2
+    one_count = (change % 2) // 1
+    
+    
+    print("$100: {}ea".format(hundred_count))
+    print("$50: {}ea".format(fifty_count))
+    print("$20: {}ea".format(twenty_count))
+    print("$10: {}ea".format(ten_count))
+    print("$5: {}ea".format(five_count))
+    print("$2: {}ea".format(two_count))
+    print("$1: {}ea".format(one_count))
+   
+
+
+# Test
+calculate_change(300, 100)
+print()
