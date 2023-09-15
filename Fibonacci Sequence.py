a74e2e79161e5a84bbd4532f2c3c2f6728022bd9
Conni2 <connie.keum@gmail.com>
2023-09-15 11:19:04 +0900

    Add description

--- Fibonacci Sequence.py
+++ Fibonacci Sequence.py
@@ -1,3 +1,7 @@
+# 6th Project - Fibonacci Sequence
+# Description: Fibonacci sequence | Purpose: Practice while statement
+# TIL: Using other variable to save "previous" value before update "previous" value into "current" for calculating next term
+
 previous = 0
 current = 1
 i = 1
