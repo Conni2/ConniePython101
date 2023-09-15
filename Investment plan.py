d93c389789f579bb2cd87c727822186512adff91
Conni2 <connie.keum@gmail.com>
2023-09-15 11:16:28 +0900

    Add description

--- Investment plan.py
+++ Investment plan.py
@@ -1,16 +1,16 @@
-#This is for comparint two different investment type (simple interest/compound interest) with different interest rate to make the best invetstment plan
-#input values are 1) initial capital 2) investment year 3)interest rate
-#The difference of interest between two plans are only displayed upto 4 decimal places
-#currency = $
+# 5th Project - Investment plan
+# Description: Compare two different investment types (simple interest/compound interest) with different interest rates | Purpose: To practice while & if statment
+# The difference of interest between two plans are only displayed upto 4 decimal places
+# TIL: How to calculate the compound rate (need to change the condition in the while statement)
 
-#입력값 받기
+#Receiving values
 capital = int(input("Initial capital:"))
 startingyear = int(input("Starting year:"))
 endingyear = int(input("Desired end year:"))
 interestrate_simple = float(input("Interest rate for simple interest:")) / 100
 interestrate_compound = float(input("Interest rate for compound interest:")) / 100
 
-#변수 정의
+#Variables
 simple = capital * (1+(endingyear-startingyear)*interestrate_simple)
 compound = capital
 year = startingyear
@@ -22,8 +22,8 @@ while year < endingyear:
 comparison = round(simple - compound, 4)
 if comparison < 0:
     comparison = abs(comparison)
-    print("Compound interest plan is more profitable than simple interest by ${}.".format(comparison))
+    print("Compound interest is more profitable than simple interest by ${}.".format(comparison))
 elif comparison == 0:
-    print("Interest from two plans is same")
+    print("Interests from two plans are same")
 else:
-    print("Simple interest plan is more profitable than compound interest plan by ${}.".format(comparison))
+    print("Simple interest is more profitable than compound interest plan by ${}.".format(comparison))
