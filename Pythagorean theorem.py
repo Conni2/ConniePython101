3ca69e78297dcfb87304f8cd926f3b92b04cbf2a
Conni2 <connie.keum@gmail.com>
2023-09-18 00:02:23 +0900

    Add description

--- Pythagorean theorem.py
+++ Pythagorean theorem.py
@@ -1,5 +1,6 @@
-#10th Project - Pythagorean theorem
+# 10th Project - Pythagorean theorem
 # Purpose: Practicing for statement
+# TIL: If we know about c using a and b, it's better not to use the for statement for code simplification
 
 for a in range(1, 1000):
     for b in range(1, 1000):
