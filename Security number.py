7b82b8a836d6201cdfd1f403268cf8357a7ce58d
Conni2 <connie.keum@gmail.com>
2023-09-27 13:31:40 +0900

    New project

--- Security number.py
+++ Security number.py
@@ -1,4 +1,3 @@
 # 14th project - Security number
 # Description: Automatic data masking for social security number | Purpose: To practice str indexing
-# TIL: List can be modified (index = desired value), but string cannot be modified.
-
+# TIL: List can be modified but string can't. To modify string, need to convert it to list
