de42e3f835766e10cad27ba094f1cb2d4b61e765
Conni2 <connie.keum@gmail.com>
2023-09-27 13:44:15 +0900

    Masked number using len, for and changing str to list

--- Security number.py
+++ Security number.py
@@ -1,3 +1,16 @@
 # 14th project - Security number
 # Description: Automatic data masking for social security number | Purpose: To practice str indexing
 # TIL: List can be modified but string can't. To modify string, need to convert it to list
+
+def number_masking (number):
+    str_to_list =[]
+    for i in range(len(number)):
+        str_to_list.append(number[i])
+    for i in range(len(str_to_list)-4, len(str_to_list)):
+        str_to_list[i] = "*"
+    masked_number = ""
+    for i in range (len(str_to_list)):
+        masked_number += str_to_list[i]
+    return masked_number
+
+print(number_masking ("980505-1234567"))
