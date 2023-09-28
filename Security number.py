8817575ee84432a27b210b706aff693c541c6d9d
Conni2 <connie.keum@gmail.com>
2023-09-28 12:42:05 +0900

    join

--- Security number.py
+++ Security number.py
@@ -17,3 +17,14 @@ print(number_masking ("980505-1234567"))
 
 #TIL: can also change str to list as below:
 #(line 6~8): str_to_list = ****list(number)**** (But this will put number/alphabet/symbol one by one in the list as an individual element)
+
+#TIL: join method // "seperator".join(list)
+
+def number_masking2 (number2):
+    strtolist = []
+    strtolist = list(number2)
+    for i in range(len(strtolist)-4, len(strtolist)):
+        strtolist[i] = "*"
+    return ''.join(strtolist)
+
+print(number_masking2("980505-1234567"))
