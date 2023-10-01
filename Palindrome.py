4c8bd086c537717807409e85dfa4e4e8e7612a92
Conni2 <connie.keum@gmail.com>
2023-10-01 14:02:48 +0900

    Korean palindrome

--- Palindrome.py
+++ Palindrome.py
@@ -1,5 +1,13 @@
 # 15th project - Palindrome
 # Purpose: To practice str indexing
 
-def ispalindrome (word):
-    
+def palindrome (word):
+    reverse = []
+    reverse = list(word)
+    for left in range(len(reverse)//2):
+        right = len(reverse)-left-1
+        if reverse[left] != reverse[right]:
+            print (False)
+        print (True)
+
+palindrome("토마토")
