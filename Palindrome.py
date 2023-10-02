3b24403d496098c112487d753e8e5d24718962d6
Conni2 <connie.keum@gmail.com>
2023-10-02 23:47:28 +0900

    English Palindrome

--- Palindrome.py
+++ Palindrome.py
@@ -2,14 +2,32 @@
 # Purpose: To practice str indexing
 # Improvement needed: Succeeded in Korean but need to change the code in case of English
 
-def palindrome ():
-    word = str(input("Please input the word:"))
+def palindromekr ():
+    krword = str(input("단어를 입력해주세요:"))
     reverse = []
-    reverse = list(word)
+    reverse = list(krword)
     for left in range(len(reverse)//2):
         right = len(reverse)-left-1
         if reverse[left] != reverse[right]:
             print (False)
-        print (True)
+        else:
+            print (True)
 
-palindrome()
+palindromekr()
+
+def palindromeeng ():
+    word = str(input("Please input the word:"))
+    reversed = []
+    reversed = list(word)
+    for left in range (len(reversed)//2):
+        right = len(reversed)-left-1
+        temp = reversed[left]
+        reversed[left] = reversed[right]
+        reversed[right] = temp
+    reversed_word = ''.join(reversed)
+    if reversed_word == word:
+        print(True)
+    else:
+        print(False)
+
+palindromeeng ()
