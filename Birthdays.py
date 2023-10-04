4a686f8cac58b0793e327a1b4d3cfc1425a86a08
Conni2 <connie.keum@gmail.com>
2023-10-04 22:16:13 +0900

    Project plan changed

---
+++ Birthdays.py
@@ -0,0 +1,14 @@
+# 18th Project: Birthdays
+# Purpose: To practice datetime module
+
+import datetime
+
+with open('Birthdays/Friends_bday.txt', 'a') as f:
+    while True:
+        name = input("Name:")
+        if name == "DONE":
+            break
+    while True:
+        birthday = 
+
+    f.write('{}: {}'.format(name, date))
--- Birthdays/Birthdays.py
+++
@@ -1,7 +0,0 @@
-# 18th Project: Birthdays
-# Purpose: To practice datetime module
-
-import datetime
-
-with open('Birthdays/Friends_bday.txt', 'a') as f:
-    f.write('Hello world')
