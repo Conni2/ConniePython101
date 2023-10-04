36877018f06b0f4255b663011550a3faaa25c9ec
Conni2 <connie.keum@gmail.com>
2023-10-04 21:53:30 +0900

    New project created

--- Average income.py
+++ Average income.py
@@ -1,3 +1,5 @@
 # 17th Project: Average income
 # Purpose: To practice reading files and process it with split/strip
 
+with open('file.txt', 'w') as f:
+    f.write("Bonjour tout le monde!")
---
+++ File creation.py
@@ -0,0 +1,6 @@
+# 17th Project: File creation
+# Purpose: Practice creating files
+
+
+with open('file.txt', 'w') as f:
+    f.write("Bonjour tout le monde!")
