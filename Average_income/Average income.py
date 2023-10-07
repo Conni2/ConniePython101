a01be1e36798eb7c9c2afab8b4ee38f16aa8c74c
Conni2 <connie.keum@gmail.com>
2023-10-07 23:14:05 +0900

    Defined variables

--- Average_income/Average income.py
+++ Average_income/Average income.py
@@ -1,3 +1,9 @@
 # 17th Project: Average income
 # Purpose: To practice reading files and process it with split/strip
+# TIL: open file -> open('file/data', 'r') as f:
 
+with open('Average_income/October_profit.txt', 'r') as f:
+    total_revenue = 0
+    total_days = 0
+    for line in f:
+        data = line.strip().split(': ')
