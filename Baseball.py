f3a68703fe386d079a0ce8a6b4682ceef31cf516
Conni2 <connie.keum@gmail.com>
2023-10-13 00:10:19 +0900

    User input

--- Baseball.py
+++ Baseball.py
@@ -10,12 +10,23 @@ def generate_numbers():
         random_num = random.randint(0,9)
         if random_num not in numbers:
             numbers.append(random_num)
+    print("Three different numbers between 0 and 9 were drawn in random order.\n")
     return(numbers)
 
-#test print(generate_numbers ())
+generate_numbers()
 
 #User input receive
 def take_guess():
     print("Please type your choice of number in order")
     numbers_user = []
-    num_1 = int(input("Input the first number:"))
+    while len(numbers_user) < 3:
+        user_num = int(input("Your number #{}:".format(len(numbers_user)+1)))
+        if user_num > 9 or user_num < 0:
+            print ("{} is out of range. Try again.".format(user_num))
+        elif user_num in numbers_user:
+            print ("{} is a duplicate number. Try again".format(user_num))
+        else:
+            numbers_user.append(user_num)
+    return(numbers_user)
+
+print(take_guess())
