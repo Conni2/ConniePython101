206b801541f42b2ea289324205f254df1b87e100
Conni2 <connie.keum@gmail.com>
2023-10-14 12:42:30 +0900

    Scoring system built

--- Baseball.py
+++ Baseball.py
@@ -13,7 +13,6 @@ def generate_numbers():
     print("Three different numbers between 0 and 9 were drawn in random order.\n")
     return(numbers)
 
-generate_numbers()
 
 #User input receive
 def take_guess():
@@ -29,4 +28,29 @@ def take_guess():
             numbers_user.append(user_num)
     return(numbers_user)
 
-print(take_guess())
+
+#print score
+#TIL: Can return two values using return(A, B), also try to simplify the code using elif(since this will filter the numbers in the first if)
+def get_score(numbers, numbers_user):
+    ball = 0
+    strike = 0
+    for i in range(len(numbers)):
+        if numbers[i] == numbers_user[i]:
+            strike += 1
+        elif numbers[i] in numbers_user:
+            ball += 1
+    return (strike, ball)
+
+
+#Game begins
+computer = generate_numbers()
+tries = 1
+
+while True:
+    user_guess = take_guess()
+    s, b = get_score(computer, user_guess)
+    print ("<#{} Inning> \n Strike: {} | Ball: {}\n".format(tries,s,b))
+    tries += 1
+    if s == 3:
+        print("Three strikes! You won the game after {} winning".format(tries-1))
+        break
